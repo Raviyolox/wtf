ce94a3c4dc99259ecf7020b754fd316fb264a00c
Raviyolox <74963613+Raviyolox@users.noreply.github.com>
2022-07-28 18:57:31 +0200

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -37,8 +37,8 @@ function MyApp({ Component, pageProps }) {
     });
   return (<>
     <Head>
-        <title>Umut Bayraktar</title>
-        <link rel="shortcut icon" href="https://media.discordapp.net/attachments/737348411568685066/954506783777493072/Umut_Bayraktar_Icon.png" type="image/x-icon" ></link>
+        <title>Switch Client</title>
+        <link rel="shortcut icon" href="" type="image/x-icon" ></link>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet" />
 
@@ -56,7 +56,7 @@ function MyApp({ Component, pageProps }) {
         <div style={{ zIndex: 99999 }} className="fixed bg-black/75 w-full h-screen flex justify-center items-center pointer-events-none">
             <div className="flex items-center gap-x-6 animate-pulse">
                 <div className="text-center">
-                    <p className="text-6xl mb-5 font-semibold">Umut Bayraktar</p>
+                    <p className="text-6xl mb-5 font-semibold">Switch Client</p>
                     <p className="uppercase text-xl font-semibold text-white"><i className="fal fa-spinner-third fa-spin" /></p>
                 </div>
             </div>
@@ -73,8 +73,8 @@ function MyApp({ Component, pageProps }) {
             <div>
               <p>❤️ Umut Bayraktar</p></div>
             <div className="mt-2 md:mt-0 flex items-center">
-              <a href="https://discord.gg/58e5H4try3" target="_blank" rel="noreferrer" className="w-full md:w-auto bg-neutral-700/5 hover:bg-neutral-700/20 px-4 py-2 rounded-md transition-all duration-200">
-                <i className="fab fa-discord mr-2" />Join My Discord
+              <a href="" target="_blank" rel="noreferrer" className="w-full md:w-auto bg-neutral-700/5 hover:bg-neutral-700/20 px-4 py-2 rounded-md transition-all duration-200">
+                <i className="fab fa-discord mr-2" />
               </a>
             </div>
 
