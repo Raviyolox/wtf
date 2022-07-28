8698c8762a57b3eec7f4db51a1e4710507943183
Raviyolox <74963613+Raviyolox@users.noreply.github.com>
2022-07-28 18:44:48 +0200

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -18,7 +18,7 @@ export default function Home() {
                 <a target="_blank" rel="norefferer" href="https://discord.com/users/615029465726320654">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-discord fa-2x" />
-                        <p className="font-semibold text-xl">Umut#6070</p>
+                        <p className="font-semibold text-xl">Darks#1337</p>
                     </div>
                 </a>
                 <a href="mailto:umutbayraktar55@gmail.com">
